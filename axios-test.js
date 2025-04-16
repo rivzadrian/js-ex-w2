8f16bef4dc99d2525c312e7e03f1b57d75c0141a
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-16 23:11:44 +0700

    fix axios and json read

--- axios-test.js
+++ axios-test.js
@@ -1,13 +1,24 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-async function getUser() {
-    try {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-        console.log(response);
+// async function getUser() {
+//     try {
+//         const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+//         console.log(response);
         
-    } catch (error) {
-        console.log("error");
+//     } catch (error) {
+//         console.log("error");
         
-    }
-}
-getUser();
+//     }
+// }
+// getUser();
+
+// GET request for remote image in node.js
+
+import axios from 'axios';
+const response = await axios({
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    responseType: "json",
+  });
+
+console.log(response.data)
--- json-read.js
+++ json-read.js
@@ -1,3 +1,10 @@
-fetch("https://jsonplaceholder.typicode.com/todos").then((response)=> response.json()).then((json)=>console.log(json));
+// fetch("https://jsonplaceholder.typicode.com/todos").then((response)=> response.json()).then((json)=>console.log(json));
 
 // https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/
+
+import fs from 'fs'
+var obj = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
+console.log(obj);
+
+
+
