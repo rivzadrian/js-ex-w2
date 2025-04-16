58502fff368563c1d3b219c3372d7fdd7fe7de04
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-16 20:06:25 +0700

    fix callback getMenu

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const userDB = { 
-    username: "joko",
-    password: "rahasia",
-    role: "admin"
+const userDB = {
+  username: "joko",
+  password: "rahasia",
+  role: "admin",
 };
 
-function loginUser(inputUsername, inputPassword, callback){
-    setTimeout(() => {
-        if (inputUsername === userDB.username && inputPassword === userDB.password){
-            callback(userDB)
-        }
-        else console.log("username atau password salah");
-    }, 500);
+function loginUser(inputUsername, inputPassword, callback) {
+  setTimeout(() => {
+    if (
+      inputUsername === userDB.username &&
+      inputPassword === userDB.password
+    ) {
+      callback(userDB);
+    } else console.log("username atau password salah");
+  }, 500);
 }
 
 //ambil role lempar pakai callback
-function getRole(user, callback){
-    setTimeout(()=>{
-        callback(user.role);
-    },500);
+function getRole(user, callback) {
+  setTimeout(() => {
+    callback(user.role);
+  }, 500);
 }
 
 // cek role, jika admin lempar dashboard admin jika bukan admin
-function getMenu(role, callback){
-    setTimeout(()=>{
-        getRole(role, function(role){
-            if (role === 'admin'){
-                console.log("WILKOMMEN TO ADMIN DASHBOARD MY LORD");
-            } 
-            else console.log("Welcome to normal people dashboard");
-            
-        })
-    },500);
+function getMenu(role, callback) {
+  setTimeout(() => {
+    getRole(role, function (role) {
+      callback(role);
+    });
+  }, 500);
 }
 
-loginUser("joko", "rahasia", function(user){
-    loginUser(user.username, user.password, function(user){
-        getMenu(user, function(user){
-            console.log(user);
-        })
-    })
-})
-
+loginUser("joko", "rahasia", function (user) {
+  loginUser(user.username, user.password, function (user) {
+    getMenu(user, function (user) {
+      if (user === "admin") {
+        console.log("WILKOMMEN TO ADMIN DASHBOARD MY LORD");
+      } else console.log("Welcome to normal people dashboard");
+    });
+  });
+});
