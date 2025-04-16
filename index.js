068a76c756c59eb2fe3735e19961df159faf0ccc
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-16 19:57:34 +0700

    feat(login):add login callback

--- index.js
+++ index.js
@@ -6,24 +6,34 @@ const userDB = {
 
 function loginUser(inputUsername, inputPassword, callback){
     setTimeout(() => {
-
+        if (inputUsername === userDB.username && inputPassword === userDB.password){
+            callback(userDB)
+        }
+        else console.log("username salah");
     }, 500);
 }
 
 //ambil role lempar pakai callback
-function getRole(user, callbac){
+function getRole(user, callback){
     setTimeout(()=>{
-
+        callback(user.role);
     },500);
 }
 
 // cek role, jika admin lempar dashboard admin jika bukan admin
 function getMenu(role, callback){
     setTimeout(()=>{
-
+        getRole(user, function(user){
+            if (user === 'admin'){
+                console.log("Wilkommen to admin dashboard");
+            } 
+        })
     },500);
 }
 
 loginUser("joko", "rahasia", function(user){
-    
+    loginUser(user.username, user.password, function(user){
+        getMenu()
+    })
 })
+
---
+++ sandbox.js
@@ -0,0 +1,7 @@
+const userDB = { 
+    username: "joko",
+    password: "rahasia",
+    role: "admin"
+};
+console.log(userDB.username);
+
