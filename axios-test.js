0b56527e7cd44e822b3ba676ae8a893651e126ad
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-16 20:25:55 +0700

    feat(axios): add axios fetch

---
+++ axios-test.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function getUser() {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+        console.log(response);
+        
+    } catch (error) {
+        console.log("error");
+        
+    }
+}
+
+getUser();
