c30e406307c47d04b14f8398cbf9545aeabf1286
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-16 19:25:48 +0700

    Base code

---
+++ index.js
@@ -0,0 +1,29 @@
+const userDB = { 
+    username: "joko",
+    password: "rahasia",
+    role: "admin"
+};
+
+function loginUser(inputUsername, inputPassword, callback){
+    setTimeout(() => {
+
+    }, 500);
+}
+
+//ambil role lempar pakai callback
+function getRole(user, callbac){
+    setTimeout(()=>{
+
+    },500);
+}
+
+// cek role, jika admin lempar dashboard admin jika bukan admin
+function getMenu(role, callback){
+    setTimeout(()=>{
+
+    },500);
+}
+
+loginUser("joko", "rahasia", function(user){
+    
+})
